f82e2c317727e3ed794da3edb89ea66662ed6e51
Daniel <i@dast.xyz>
2022-12-01 15:24:53 +0100

    run all motors in parallel

--- microproccesor/turning-servor-uart.ino
+++ microproccesor/turning-servor-uart.ino
@@ -1,6 +1,10 @@
+#define ARDUINO 101
+
 #include <TMCStepper.h>     // TMCstepper - https://github.com/teemuatlut/TMCStepper
 #include <SoftwareSerial.h> // Software serial for the UART to TMC2209 - https://www.arduino.cc/en/Reference/softwareSerial
 #include <Streaming.h>      // For serial debugging output - https://www.arduino.cc/reference/en/libraries/streaming/
+#include <MultiStepper.h>
+#include <AccelStepper.h>
 
 #define DIR_PIN 0           // Direction - WHITE
 #define STEP_PIN 1          // Step - ORANGE
@@ -10,18 +14,30 @@
 #define DRIVER_ADDRESS 0b00 // TMC2209 Driver address according to MS1 and MS2
 #define R_SENSE 0.11f       // SilentStepStick series use 0.11 ...and so does my fysetc TMC2209 (?)
 
-const byte MOTOR_PIN_OFFSETS[] = {2, 9, 14}; // each offset is summed with the pin positions above to give the actual position on the arduino
+constexpr uint32_t steps_per_mm = 80;
 
+const byte MOTOR_PIN_OFFSETS[] = {2, 9, 14}; // each offset is summed with the pin positions above to give the actual position on the arduino
+const int sizeMotors = sizeof(MOTOR_PIN_OFFSETS) / sizeof(byte);
+
+// simulate serial interfaces using digital pins because arduino doesn't have enough physical serial interfaces
 SoftwareSerial SoftSerials[] = {
     SoftwareSerial(SW_RX + MOTOR_PIN_OFFSETS[0], SW_TX + MOTOR_PIN_OFFSETS[0]),
     SoftwareSerial(SW_RX + MOTOR_PIN_OFFSETS[1], SW_TX + MOTOR_PIN_OFFSETS[1]),
     SoftwareSerial(SW_RX + MOTOR_PIN_OFFSETS[2], SW_TX + MOTOR_PIN_OFFSETS[2]),
 };
 
+// set tmc driver settings
 TMC2209Stepper TMCdrivers[] = {
     TMC2209Stepper((Stream *)&SoftSerials[0], R_SENSE, DRIVER_ADDRESS),
     TMC2209Stepper((Stream *)&SoftSerials[1], R_SENSE, DRIVER_ADDRESS),
     TMC2209Stepper((Stream *)&SoftSerials[2], R_SENSE, DRIVER_ADDRESS),
+};
+
+// run motors in parallel
+AccelStepper steppers[] = {
+    AccelStepper(AccelStepper::DRIVER, MOTOR_PIN_OFFSETS[0] + STEP_PIN, MOTOR_PIN_OFFSETS[0] + DIR_PIN),
+    AccelStepper(AccelStepper::DRIVER, MOTOR_PIN_OFFSETS[1] + STEP_PIN, MOTOR_PIN_OFFSETS[1] + DIR_PIN),
+    AccelStepper(AccelStepper::DRIVER, MOTOR_PIN_OFFSETS[2] + STEP_PIN, MOTOR_PIN_OFFSETS[2] + DIR_PIN),
 };
 
 int accel;
@@ -31,49 +47,34 @@ bool dir = false;
 
 void setup()
 {
-    int sizeMotors = sizeof(MOTOR_PIN_OFFSETS) / sizeof(byte);
     for (int i = 0; i < sizeMotors; i++)
     {
         SoftSerials[i].begin(11520);      // initialize software serial for UART motor control
         TMCdrivers[i].beginSerial(11520); // Initialize UART
-
-        pinMode(EN_PIN + MOTOR_PIN_OFFSETS[i], OUTPUT); // Set pinmodes
-        pinMode(STEP_PIN + MOTOR_PIN_OFFSETS[i], OUTPUT);
-        pinMode(DIR_PIN + MOTOR_PIN_OFFSETS[i], OUTPUT);
-        digitalWrite(EN_PIN + MOTOR_PIN_OFFSETS[i], LOW); // Enable TMC2209 board
-
-        TMCdrivers[i].begin();          // UART: Init SW UART (if selected) with default 115200 baudrate
-        TMCdrivers[i].toff(5);          // Enables driver in software
-        TMCdrivers[i].rms_current(500); // Set motor RMS current
-        TMCdrivers[i].microsteps(256);  // Set microsteps
+        TMCdrivers[i].begin();            // UART: Init SW UART (if selected) with default 115200 baudrate
+        TMCdrivers[i].toff(5);            // Enables driver in software
+        TMCdrivers[i].rms_current(400);   // Set motor RMS current
+        TMCdrivers[i].microsteps(16);     // Set microsteps
 
         TMCdrivers[i].en_spreadCycle(false);
         TMCdrivers[i].pwm_autoscale(true); // Needed for stealthChop
+
+        steppers[i].setMaxSpeed(800 * steps_per_mm);      // 100mm/s @ 80 steps/mm
+        steppers[i].setAcceleration(1000 * steps_per_mm); // 2000mm/s^2
+        steppers[i].setEnablePin(EN_PIN + MOTOR_PIN_OFFSETS[i]);
+        steppers[i].setPinsInverted(false, false, true);
+        steppers[i].enableOutputs();
     }
 }
 
 void loop()
 {
-    int sizeMotors = sizeof(MOTOR_PIN_OFFSETS) / sizeof(byte);
-    for (int n = 0; n < sizeMotors; n++)
+    for (int i = 0; i < sizeMotors; i++)
     {
-        accel = 10000;          // Speed increase/decrease amount
-        maxSpeed = 50000;       // Maximum speed to be reached
-        speedChangeDelay = 100; // Delay between speed changes
-
-        for (long i = 0; i <= maxSpeed; i = i + accel)
-        {                             // Speed up to maxSpeed
-            TMCdrivers[n].VACTUAL(i); // Set motor speed
-            delay(100);
+        if (steppers[i].distanceToGo() == 0)
+        {
+            steppers[i].move(100 * steps_per_mm); // Move 100mm
         }
-
-        for (long i = maxSpeed; i >= 0; i = i - accel)
-        { // Decrease speed to zero
-            TMCdrivers[n].VACTUAL(i);
-            delay(100);
-        }
-
-        dir = !dir;               // REVERSE DIRECTION
-        TMCdrivers[n].shaft(dir); // SET DIRECTION
+        steppers[i].run();
     }
 }
